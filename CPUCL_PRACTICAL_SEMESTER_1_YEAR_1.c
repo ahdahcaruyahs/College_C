91f0460ab05e839525d96b5da922ed0a64a70aba
LMXAB3KS <84719688+LMXAB3KS@users.noreply.github.com>
2023-12-20 12:41:20 +0530

    Create CPUCL_PRACTICAL_SEMESTER_1_YEAR_1.c

---
+++ CPUCL_PRACTICAL_SEMESTER_1_YEAR_1.c
@@ -0,0 +1,10 @@
+//Date: 12 December 2023
+//Note: these were the questions that We had to perform based on where we were sitting. People sitting on the right side of the row had to perform set 1.
+
+//Set 2:
+//WAP to print any 5 variables
+//WAP to find out whether a number is an ormstrong number number or not
+
+//Set 1:
+//Wap to find out whether a number is odd or even
+//Wap to find out whether a number is a factorial or not
